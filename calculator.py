57dc5b6778c8d75e53785022655c672b7d64c24d
Thomas <tvanerven@intothesource.com>
2021-11-28 12:04:20 +0100

    Steps 2 and 3 from part A.

--- calculator.py
+++ calculator.py
@@ -1,31 +1,9 @@
 import datetime
 from decimal import Decimal, InvalidOperation
+from typing import Union
 
 
-class Bonus:
-
-    def __init__(
-            self,
-            start_date: str,
-            end_date: str,
-            value: Decimal,
-            user_id: int,
-            *args,
-            **kwargs
-    ):
-        """
-        A bonus, representing a bonus to be calculated with. Expected formats:
-        start_date: A string, formatted as "dd-mm-yyyy"
-        end_date: A string, formatted as "dd-mm-yyyy"
-        value: a Decimal number
-        user_id: A integer, representing an ID in the datebase.
-        """
-        self.start_date = self._validate_datetime(start_date)
-        self.end_date = self._validate_datetime(end_date)
-        self.user = user_id
-        self.value = value
-
-    def _validate_datetime(self, date_string):
+def validate_datetime(date_string):
         try:
             return datetime.datetime.strptime(
                 date_string, '%d-%m-%Y'
@@ -35,18 +13,65 @@ class Bonus:
                 "The date you have entered is incorrect, likely misformatted."
             )
 
-    def _validate_value(self, value):
-        try:
-            return Decimal(value)
-        except InvalidOperation:
-            raise InvalidOperation(
-                "The value could not be converted to a Decimal"
-            )
+
+def validate_value(value):
+    try:
+        return Decimal(value)
+    except InvalidOperation:
+        raise InvalidOperation(
+            "The value could not be converted to a Decimal"
+        )
 
 
-    @property
-    def time_delta(self):
-        return self.end_date - self.start_date
+class Bonus:
+
+    def __init__(
+            self,
+            identifier: int,
+            start_date: str,
+            end_date: str,
+            value: Decimal,
+            user_id: int,
+            is_percentage: bool = False,
+            *args,
+            **kwargs
+    ):
+        """
+        A bonus, representing a bonus to be calculated with. Expected formats:
+        id: An id to provide a unique reference per bonus.
+        start_date: A string, formatted as "dd-mm-yyyy"
+        end_date: A string, formatted as "dd-mm-yyyy"
+        value: a Decimal number
+        user_id: A integer, representing an ID in the datebase.
+        is_percentage: A boolean, to show if the value is a percentage or
+        strict value
+        """
+        self.identifier = identifier
+        self.start_date = validate_datetime(start_date)
+        self.end_date = validate_datetime(end_date)
+        self.user = user_id
+        self.value = validate_value(value)
+        self.is_percentage = is_percentage
+
+
+class Salary:
+
+    def __init__(
+            self,
+            identifier: int,
+            user_id: int,
+            value: Union[Decimal, int],
+            *args,
+            **kwargs
+    ):
+        """A table representing a salary.
+        identifier: An identifier in a table
+        user_id: A foreignkey relating to user
+        value: A value assumed to be the monthly salary
+        """
+        self.identifier = identifier
+        self.user_id = user_id
+        self.value = validate_value(value)
 
 
 class BonusCalculator:
@@ -56,23 +81,17 @@ class BonusCalculator:
             bonus_list: list[Bonus],
             start_date: str,
             end_date: str,
+            salaries: list[Salary] = [],
             *args,
             **kwargs
     ):
         self._bonus_list = bonus_list
-        self.start_date = self._validate_datetime(start_date)
-        self.end_date = self._validate_datetime(end_date)
-        self.result = self._calculate_bonus()
-
-    def _validate_datetime(self, date_string):
-        try:
-            return datetime.datetime.strptime(
-                date_string, '%d-%m-%Y'
-            )
-        except ValueError:
-            raise ValueError(
-                "The date you have entered is incorrect, likely misformatted."
-            )
+        self._salaries = salaries
+        self.start_date = validate_datetime(start_date)
+        self.end_date = validate_datetime(end_date)
+        self.value_bonus = self._calculate_value_bonuses()
+        self.percentage_bonus = self._calculate_percentage_bonus()
+        self.total_bonus = self._calculate_total_bonus()
 
     def _get_bonus_list(self) -> list[Bonus]:
         """Method to 'simulate' a database call simply to return
@@ -80,12 +99,42 @@ class BonusCalculator:
         """
         return self._bonus_list
 
-    def _calculate_bonus(self) -> Decimal:
+    def _get_salaries_for_user(self, bonus: Bonus) -> Decimal:
+        """Another method to 'simulate' a database call."""
+        for salary in self._salaries:
+            if salary.user_id == bonus.user:
+                return salary
+
+    def _calculate_percentage_bonus(self) -> Decimal:
+        result = {}
+        bonuses = self._get_bonus_list()
+        percentage_bonuses = [
+            percentage_bonus for percentage_bonus in bonuses
+            if percentage_bonus.is_percentage
+        ]
+        for bonus in percentage_bonuses:
+            salary_for_user = self._get_salaries_for_user(bonus)
+            percentage = Decimal(self._calculate_bonus_amount(bonus))
+            result[bonus.identifier] = \
+                (percentage / 100 ) * salary_for_user.value
+        return self._clean_result(result)
+
+    def _calculate_value_bonuses(self) -> Decimal:
         result = Decimal(0)
         bonuses = self._get_bonus_list()
-        for bonus in bonuses:
+        value_bonuses = [
+            value_bonus for value_bonus in bonuses
+            if value_bonus.is_percentage is False
+        ]
+        for bonus in value_bonuses:
             result += Decimal(self._calculate_bonus_amount(bonus))
         return self._clean_result(result)
+
+    def _calculate_total_bonus(self) -> Decimal:
+        bonus_total = self.value_bonus
+        for key, value in self.percentage_bonus.items():
+            bonus_total += value
+        return bonus_total
 
     def _calculate_bonus_amount(self, bonus: Bonus) -> Decimal:
         if (
@@ -132,6 +181,12 @@ class BonusCalculator:
         partial_bonus = partial_bonus_time.days * value_per_day
         return partial_bonus
 
-    def _clean_result(self, result: float) -> Decimal:
-        return round(result.quantize(Decimal('0.01')))
+    def _clean_result(self, result: Union[Decimal, dict]) -> Decimal:
+        if isinstance(result, Decimal):
+            return round(result.quantize(Decimal('0.01')))
+        if isinstance(result, dict):
+            cleaned_result = {}
+            for key, value in result.items():
+                cleaned_result[key] = round(value.quantize(Decimal('0.01')))
+            return cleaned_result
 
--- tests.py
+++ tests.py
@@ -1,19 +1,27 @@
 from unittest import TestCase
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 
-from calculator import Bonus, BonusCalculator
+from calculator import (
+    Bonus,
+    BonusCalculator,
+    Salary,
+    validate_datetime,
+    validate_value
+)
 
 
 class BonusCalculatorTestCase(TestCase):
 
     def test_scripted_scenario(self):
         first_bonus = Bonus(
+            identifier=1,
             start_date='01-01-2019',
             end_date='01-01-2020',
             value=100,
             user_id=1
         )
         second_bonus = Bonus(
+            identifier=2,
             start_date='01-01-2020',
             end_date='01-01-2021',
             value=200,
@@ -24,7 +32,56 @@ class BonusCalculatorTestCase(TestCase):
             start_date='01-07-2019',
             end_date='01-01-2021'
         )
-        self.assertEqual(
-            bonus.result,
-            Decimal('250.00')
+        self.assertEqual(bonus.value_bonus, Decimal('250.00'))
+        self.assertEqual(bonus.total_bonus, Decimal('250.00'))
+
+    def test_percentage_scenario(self):
+        first_bonus = Bonus(
+            identifier=1,
+            start_date='01-01-2019',
+            end_date='01-01-2020',
+            value=12,
+            user_id=1,
+            is_percentage=True
         )
+        second_bonus = Bonus(
+            identifier=2,
+            start_date='01-01-2020',
+            end_date='01-01-2021',
+            value=200,
+            user_id=1,
+        )
+        salary = Salary(
+            identifier=1,
+            start_date='01-01-2019',
+            end_date='01-01-2021',
+            value=Decimal('1000'),
+            user_id=1
+        )
+        invalid_salary = Salary(
+            identifier=2,
+            start_date='01-01-2019',
+            end_date='01-01-2021',
+            value=Decimal('1000'),
+            user_id=2
+        )
+        bonus = BonusCalculator(
+            bonus_list=[first_bonus, second_bonus],
+            start_date='01-07-2019',
+            end_date='01-01-2021',
+            salaries=[salary, invalid_salary]
+        )
+        self.assertEqual(bonus.value_bonus, 200)
+        self.assertEqual(bonus.percentage_bonus, {1: Decimal('60')})
+        self.assertEqual(bonus.total_bonus, Decimal('260.00'))
+
+    def test_exceptions(self):
+        with self.assertRaises(ValueError):
+            validate_datetime('01/01/2019')
+            validate_datetime('50-45-1')
+            validate_datetime(-1)
+            validate_datetime(20)
+        with self.assertRaises(InvalidOperation):
+            validate_value('something')
+            validate_value(6.4)
+            validate_value(Bonus())
